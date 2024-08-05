f45dbc95180c44f9dabb7771e4b86f7d293d489f
datpham2 <datpham3141@gmail.com>
2024-08-05 15:52:32 +0700

    changed in homepage of the homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,46 +8,93 @@ export default function Home() {
             .then(res => res.json())
             .then(function (data) {
                 setStocks(data)
-                setInterval(function() {
-                    let priceElements = document.querySelectorAll('.stock-info .stock-price .price');
-                    let changeElements = document.querySelectorAll('.stock-info .stock-price .change');
-                    console.log('hello')
-                    console.log(priceElements)
-                }, 5000
-                )
+
+                // document.querySelectorAll('.container .stock .stock-info .stock-price .change').forEach(changeElement => {
+                //     changeElement.classList.toggle('negative', parseFloat(changeElement.innerText) < 0)
+                // }
+                // )
             })
+            .catch(function (error) {
+                console.log(error)
+            })
+
+            
+
+            // setInterval(() => {
+            //     setStocks(prevStocks => {
+            //         const updatedStocks = prevStocks.map(stock => {
+            //             // generate a random number between -20 and 20
+            //             const randomChange = (Math.random() * 40) - 20
+            //             const updatedPrice = parseFloat((stock.price + randomChange).toFixed(2))
+
+            //             return {
+            //                 ...stock,
+            //                 price: updatedPrice,
+            //                 change: randomChange
+            //             }
+            //         })
+
+            //         let changeElements = document.querySelectorAll('.container .stock .stock-info .stock-price .change')
+
+            //         for (let i = 0; i < updatedStocks.length; i++) {
+            //             changeElements[i].innerText = `${updatedStocks[i].change > 0 ? '+' : '-'}${Math.abs(updatedStocks[i].change).toFixed(2)}` 
+            //             changeElements[i].classList.toggle('negative', updatedStocks[i].change < 0)
+            //         }
+
+
+            //         return updatedStocks;
+            //     })
+            // }, 4000)
+
+            setInterval(() => {
+                setStocks(prevStocks => {
+                    const updatedStocks = prevStocks.map(stock => {
+                        // generate a random number between -5 and 5
+                        const randomChange = (Math.random() * 10) - 5
+                        const updatedPrice = parseFloat((stock.price + randomChange).toFixed(2))
+
+                        return {
+                            ...stock,
+                            price: updatedPrice,
+                            change: randomChange
+                        }
+                    })
+
+                    return updatedStocks;
+                })
+            }, 4000)
     }
-    , [])
+        , [])
 
     return (
         <div>
-            <div className="container">
-                <h1>Welcome to Stock Homepage</h1>
-                <p>Check out the latest stock information below:</p>
-                <div className="d-flex justify-content-center 
-                flex-wrap
-                ">
-                    {
-                        stocks.map(stock => (
-                            <div key={stock.id} className="stock"  data-aos="fade-right">
-                                <div className="stock-image">
-                                    <img src={stock.image} alt="Stock Image" />
-                                </div>
-                                <div className="stock-info">
-                                    <h2 className="stock-name">
-                                        {stock.symbol}
-                                    </h2>
-                                    <div className="stock-price">
-                                        <p className="price">{stock.price}</p>
-                                        <p className={stock.change > 0 ? "change positive" : "change negative"}>
-                                            {stock.change > 0 ? "+" : "-"}{Math.abs(stock.change)} ({((stock.change / stock.price) * 100).toFixed(2)}%)
-                                        </p>
-                                    </div>
-                                </div>
+            <h1>Welcome to Stock Homepage</h1>
+            <p>Check out the latest stock information below:</p>
+            <div className="
+            ">
+                {
+                    stocks.map(stock => (
+                        <div key={stock.id} className="stock" data-aos="fade-right">
+                            {/* <div className="stock-image">
+                                <img src={stock.image} alt="Stock Image" />
                             </div>
-                        ))
-                    }
-                </div>
+                            <div className="stock-info">
+                                <h2 className="stock-name">
+                                    {stock.symbol}
+                                </h2>
+                                <div className="stock-price">
+                                    <p className="price">{stock.price}</p>
+                                    <p className= {`change ${stock.change > 0 ? '' : 'negative'}`}>{stock.change > 0 ? '+' : ''}{stock.change.toFixed(2)} ({stock.change > 0 ? '+' : ''}{(stock.change / stock.price * 100).toFixed(2)}%)</p>
+                                </div>
+                            </div> */}
+                            <div className='stock-info'>
+                                <p className='symbol'>{stock.symbol}</p>
+                                <p className='price'>${stock.price}</p>
+                                <p className={`change ${stock.change > 0 ? '' : 'negative'}`}>{stock.change > 0 ? '+' : ''}{stock.change.toFixed(2)} ({stock.change > 0 ? '+' : ''}{(stock.change / stock.price * 100).toFixed(2)}%)</p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
